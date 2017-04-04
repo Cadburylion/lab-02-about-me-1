2e797ae65f2ac4bce4286b301795a966bc4651eb
Spencer Gietzen <Speng1516@comcast.net>
2017-04-04 13:42:42 -0700

    Added my own questions

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict'; /* Do this for every JavaScript file for now on forever! */
 
-console.log('Dasdasdas adsdas das.');
+var q1 = prompt('Do you think that I like programming?').trim().toLowerCase();
+console.log('User answer: ' + q1 + '\nMy answer: yes' + '\nAnswered correctly: ' + (q1 == 'yes'));
 
-var sushi = prompt('Do you think I like sushi?');
+var q2 = prompt('Have I ever been to Mexico?').trim().toLowerCase();
+console.log('User answer: ' + q2 + '\nMy answer: no' + '\nAnswered correctly: ' + (q2 == 'no'));
 
-if(sushi.trim().toLowerCase() == 'yes') {
-  alert('You were correct.');
-} else if(sushi.trim().toLowerCase() == 'no') {
-  alert('WRONG!');
-} else {
-  alert('?????????');
-}
+var q3 = prompt('Am I over 6\' tall?').trim().toLowerCase();
+console.log('User answer: ' + q3 + '\nMy answer: no' + '\nAnswered correctly: ' + (q3 == 'no'));
+
+var q4 = prompt('Do I know C++ like the back of my hand?').trim().toLowerCase();
+console.log('User answer: ' + q4 + '\nMy answer: no' + '\nAnswered correctly: ' + (q4 == 'no'));
+
+var q5 = prompt('Do I own two beagles?').trim().toLowerCase();
+console.log('User answer: ' + q5 + '\nMy answer: yes' + '\nAnswered correctly: ' + (q5 == 'yes'));
