8700919f81890d6c1466d1bc282414f126775034
Spencer Gietzen <Speng1516@comcast.net>
2017-04-05 18:25:31 -0700

    Added question 7, made a for loop that does 6 and 7. Edited my story a tiny bit, ready to turn in now.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 var totalCorrect = 0;
 var questionArr = ['Do you think that I like programming?', 'Have I ever been to Mexico?', 'Am I over 6\' tall?', 'Do I know C++ like the back of my hand?', 'Do I own two beagles?'];
 var myAnswerArr = ['yes', 'no', 'no', 'no', 'yes'];
+var theirAns;
 
 for(var questionNumber = 0; questionNumber < questionArr.length; questionNumber++) {
   var theirAns = prompt(questionArr[questionNumber]);
   if(!theirAns) { //Empty string if they just hit OK
-    theirAns = 'no input supplied';
+    theirAns = '[no input supplied]';
   } else if(theirAns !== null) { //trim() throws error when the user presses cancel
     theirAns = theirAns.trim().toLowerCase();
   }
@@ -21,49 +22,57 @@ for(var questionNumber = 0; questionNumber < questionArr.length; questionNumber+
   console.log('Question number: ' + (questionNumber + 1) + '\nUser answer: ' + theirAns + '\nMy answer: ' + myAnswerArr[questionNumber] + '/' + myAnswerArr[questionNumber].substring(0,1) + '\nAnswered correctly: ' + (checkForBothAns));
 }
 
-questionNumber++;
-var allowedTries = 4;
-var question6 = 'Can you guess the number I\'m thinking of between 0 and 25?';
-var myAns = Math.floor(Math.random() * 25);
-var running = true;
-var theirAnsTo6 = '';
-var stringOfAttempts = '';
+var questionsTriesAnswerArr = [['Can you guess the number I\'m thinking of between 0 and 25?', 4, Math.floor(Math.random() * 25)], ['What is one state I have been to besides Washington?', 6, ['oregon', 'california', 'arizona', 'new york']]];
+var running;
+var stringOfAttempts;
+var allowedTries;
+var myAns;
+var question;
 
-for(var theirTries = 0; theirTries < allowedTries && running; theirTries++) {
-  theirAnsTo6 = prompt(question6);
-  if(!theirAnsTo6) {
-    theirAnsTo6 = 'no input supplied';
-  } else if(theirAnsTo6 !== null) {
-    theirAnsTo6 = theirAnsTo6.trim().toLowerCase();
-  }
+for(questionNumber = 6; questionNumber < 8; questionNumber++) {
+  running = true;
+  stringOfAttempts = '';
+  question = questionsTriesAnswerArr[questionNumber - 6][0];
+  allowedTries = questionsTriesAnswerArr[questionNumber - 6][1];
+  myAns = questionsTriesAnswerArr[questionNumber - 6][2];
 
-  stringOfAttempts += theirAnsTo6 + ' '; //Gather a long string of all their attempts to print in the console
-  var output = 'Question number: ' + questionNumber + '\nUser answer(s): ' + stringOfAttempts + '\nMy answer: ' + myAns + '\nAnswered correctly: ' + ((theirAnsTo6 == myAns) || (theirAnsTo6 == myAns)) + '\nTotal guesses: ' + (theirTries + 1);
-  theirAnsTo6 = parseInt(theirAnsTo6);
+  for(var theirTries = 0; theirTries < allowedTries && running; theirTries++) {
+    theirAns = prompt(question);
+    if(!theirAns) {
+      theirAns = '[no input supplied]';
+    } else if(theirAns !== null) {
+      theirAns = theirAns.trim().toLowerCase();
+    }
 
-  if(theirAnsTo6 === myAns) {
-    alert('You guessed it! It took you ' + (theirTries + 1) + ' tries!');
-    console.log(output);
-    totalCorrect++;
-    running = false;
-  } else if(theirTries === allowedTries - 1) {
-    alert('You ran out of tries! Too bad.');
-    console.log(output);
-    running = false;
-  } else if(theirAnsTo6 === NaN) {
-    alert('You didn\'t guess a number!');
-  } else if(theirAnsTo6 < myAns) {
-    alert('Guess higher!');
-  } else if (theirAnsTo6 > myAns) {
-    alert('Guess lower!');
-  } else {
-    alert('Incorrect input! You lost a try!');
+    var correct = false;
+    if(myAns.toString().includes(theirAns)) {
+      correct = true;
+    }
+
+    stringOfAttempts += theirAns + ' '; //Gather a long string of all their attempts to print in the console
+    var output = 'Question number: ' + questionNumber + '\nUser answer(s): ' + stringOfAttempts + '\nMy answer: ' + myAns.toString() + '\nAnswered correctly: ' + correct + '\nTotal guesses: ' + (theirTries + 1);
+    theirAns = parseInt(theirAns);
+
+    if(correct) {
+      alert('You guessed it! It took you ' + (theirTries + 1) + ' tries!');
+      console.log(output);
+      totalCorrect++;
+      running = false;
+    } else if(theirTries === allowedTries - 1) {
+      alert('You ran out of tries! Too bad.');
+      console.log(output);
+      running = false;
+    } else if(theirAns == NaN || theirAns == null || theirAns == undefined) {
+      alert('Incorrect input! You lost a try!');
+    } else if(theirAns < myAns) {
+      alert('Guess higher!');
+    } else if (theirAns > myAns) {
+      alert('Guess lower!');
+    } else {
+      alert('Nope, sorry! Try again!');
+    }
   }
 }
-
-questionNumber++;
-
-
 
 console.log('User got ' + totalCorrect + ' out of 7 questions correct.');
 alert('You got ' + totalCorrect + ' out of 7 questions correct.');
